07de50f4a7498019829eff557c0fc9ddc023c25b
Сергей <sergej@MacBook-Pro-Sergej.local>
2023-12-03 21:40:48 +0300

    lab1

---
+++ Lab1/task1.py
@@ -0,0 +1,26 @@
+# tack1
+# name = input('введите имя своего питомца: ')
+# print(len(name))
+
+# tack2
+# print("\"Осуждаю, сударь!” - возгласил Акакий")
+
+# tack3
+# print('хочу играть в майнкрафт с юрой'*5)
+# print('rob\t', end='')
+# print('lox')
+
+# tack4
+# name = input('введите строку')
+# print(name[-1])
+
+# tack5
+# print('JOJOandDIO'[0:4])
+
+
+
+
+
+
+
+
